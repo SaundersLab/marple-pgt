6fe19bb81b84d94cfdc05c66939f2da07a4850cf
tonytony99 <anthony.bryan@outlook.com>
2022-11-25 11:22:45 +0000

    add script to run most of the example from readme

---
+++ run_example.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+[ -z $SLURM_JOBID ] && is_on_slurm=false || is_on_slurm=true
+
+$is_on_slurm || script=$0
+$is_on_slurm && script=$(scontrol show job $SLURM_JOBID | awk -F= '/Command=/{print $2}' | cut -d" " -f1)
+marple_pgt_dir=$(cd $(dirname "$script"); pwd)
+
+"$marple_pgt_dir"/reset_example.sh
+cd "$marple_pgt_dir"/example
+../src/reads_to_cds_concat.sh --trim no uk20/uk20.fastq cz2/cz2.fastq
+../src/cds_concat_to_tree_imgs.sh \
+    --start ../data/12_samples_276_genes_cds.fa.gz \
+    --out_dir tree \
+    --name 2_new_samples \
+    */*_cds_concat.fasta
